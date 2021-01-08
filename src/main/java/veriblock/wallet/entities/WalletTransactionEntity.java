63c3693ff1348bb6809c3795e22c553ef778a93c
BernatCarbo <bernatgarciacarbo@gmail.com>
2021-01-08 16:00:11 +0100

    Copyright year update

--- src/main/java/veriblock/wallet/entities/WalletTransactionEntity.java
+++ src/main/java/veriblock/wallet/entities/WalletTransactionEntity.java
@@ -1,11 +1,5 @@
 // VeriBlock GUI Wallet
-// Copyright 2017-2020 Xenios SEZC
-// All rights reserved.
-// https://www.veriblock.org
-// Distributed under the MIT software license, see the accompanying
-// file LICENSE or http://www.opensource.org/licenses/mit-license.php.
-// VeriBlock GUI Wallet
-// Copyright 2017-2020 Xenios SEZC
+// Copyright 2017-2021 Xenios SEZC
 // All rights reserved.
 // https://www.veriblock.org
 // Distributed under the MIT software license, see the accompanying
